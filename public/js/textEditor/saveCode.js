bcbdc9b8f94b16d63d170cd15496157cffac5a1e
alsuga <ing.alsuga@gmail.com>
2015-01-21 14:28:39 -0500

    solucionado error que no permitía guardar código más de una vez

--- public/js/textEditor/saveCode.js
+++ public/js/textEditor/saveCode.js
@@ -1,24 +1,31 @@
+postCallback = function (d, textStatus, jqXHR) {
+  document.getElementById("textAreaCompileError").value = d.msg;
+  if(!d.err){
+    $('#compilar').removeAttr('disabled');
+    $('#cname').text(name);
+    saved = true;
+  }
+}
+
 function guardarCodigo(){
   code = editor.getValue();
   var name = $('#cname').text();
+  var saved = false;
   if(name == " Unsaved "){
     bootbox.prompt("Ingrese el nombre del archivo", function(result) {
       if(result !== null){
         name = result;
         if(name.length > 0){
-          jQuery.post( document.URL+"/saveCode", {source: code, fileName : name},function (d, textStatus, jqXHR) {
-            document.getElementById("textAreaCompileError").value = d.msg;
-            if(!d.err){
-              $('#compilar').removeAttr('disabled');
-              $('#cname').text(name);
-            }
-          });
+          jQuery.post( document.URL+"/saveCode", {source: code, fileName : name}, postCallback);
         }else{
           bootbox.alert("Debe ingresar un nombre");
         }
 
       }
     });
+  }
+  if(saved){
+    jQuery.post( document.URL+"/saveCode", {source: code, fileName : name}, postCallback);
   }
   //name = $("input:text").val();
 }
