c97d6a899d75f0f41f57a04360684b41d7a3d06c
alsuga <ing.alsuga@gmail.com>
2015-02-16 21:27:27 -0500

    cambios en compileAndRun

--- routes/textEditor.js
+++ routes/textEditor.js
@@ -4,7 +4,6 @@ var fs   = require('fs'),
 
 /* GET home page. */
 
-
 module.exports = function(app,passport){
   app.namespace('/compiler',function(){
     app.get('/textEditor', function(req, res) {
@@ -18,33 +17,31 @@ module.exports = function(app,passport){
 
     app.post('/compileAndRun', function(req, res) {
       var source = req.body.source;
-      if( source.search(/.*system\(.*/) != -1){
-        var response = {};
+      if( source.search(/.*system\(.*/) != -1) {
+        var response = {err : true, msg};
         response.err = true;
         response.msg = 'No esta permitido hacer llamados al sistema';
         res.json(response);
         return;
       }
-      fs.writeFileSync('./codes/'+req.user.code+'.cu',req.body.source,function(err){
+      var code = './code/' + req.user.code,
+          file = code + '.cu';
+      fs.writeFileSync(file,source, function(err){
         var response = {};
         if(err){
           console.log(err);
           response.err=true;
           response.msg='Error al guardar el archivo';
           res.json(response);
+          return;
         }
       });
-      var child, code = './code/' + req.user.code,
-          com = 'nvcc ./code/' +req.user.code +'.cu -o ' + './cuda/' + req.user.code;
+      var comp = 'nvcc' + code + '.cu -o ' + code;
 
-      child = execSync(comp,
-      function (error, stdout, stderr) {
+      var child = execSync(comp, ,function (error, stdout, stderr) {
         if (error) {
           console.log('compile error');
           console.log(error);
-          console.log(error.stack);
-          console.log('Error code compile: '+error.code);
-          console.log('Signal received compile: '+error.signal);
           res.send("Compile Error\nError code: "+error.code+"\nSignal received run: "+error.signal+"\nError: "+ stderr);
           return;
         }
@@ -53,23 +50,20 @@ module.exports = function(app,passport){
           res.send(stderr);
           return;
         }
-        console.log('Compile STDOUT1: '+stdout);
-        var child2 = execSync("./codes/" + req.user.code +"/"+req.user.code,{ timeout: 1000, killSignal: 'SIGTERM'}, function (error, stdout, stderr) {
-          if (error) {
-            console.log('run error');
-            console.log(error.stack);
-            console.log('Error code run:'+error.code);
-            console.log('Signal received run: '+error.signal);
-            res.send("Run time error\nError code: "+error.code+"\nSignal received run: "+error.signal);
-            return;
-          }
-          console.log('Run STDERR: '+stderr);
-          if(stderr.lenght > 0)
-            res.send(stderr);
-          else
-            res.send(stdout);
-        });
+      });
 
+      console.log('Compile STDOUT1: '+stdout);
+      var child2 = execSync(code, { timeout: 1000, killSignal: 'SIGKILL'}, function (error, stdout, stderr) {
+        if (error) {
+          console.log('run error');
+          console.log(error.stack);
+          res.send("Run time error\nError code: "+error.code+"\nSignal received run: "+error.signal);
+          return;
+        }
+        // agregar child2.onKillSignal
+        console.log('Run STDERR: '+stderr);
+        if(stderr.lenght > 0)
+          res.send(stderr + '\n-------------------------\n' + stdout);
       });
 
     });
